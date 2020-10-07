33dcd653820aa9729676437e22cd99ae9e8d0305
PandaAwAke <191220080@smail.nju.edu.cn>
2020-10-07 23:54:31 +0800

    account_1

--- src/components/AccountComps/Account.vue
+++ src/components/AccountComps/Account.vue
@@ -1,46 +1,32 @@
 <template>
   <div id="info">
     <h2>账户信息</h2>
+    <Button v-on:click="tableShow">test-show</Button>
     <form>
-      <div id="first-name">
-        <label>姓:</label>
-        <span>{{firstName}}</span>
-
-      </div>
-      <div id="last-name">
-        <label>名:</label>
-        <span>{{lastName}}</span>
-      </div>
-      <div id="phone-number">
-        <label>手机号:</label>
-        <span>{{phoneNumber}}</span>
-      </div>
-      <div id="email">
-        <label>电子邮箱:</label>
-        <span>{{email}}</span>
-      </div>
-      <div id="address">
-        <label>地址:</label>
-        <span>{{address}}</span>
-      </div>
-      <div id="card-info">
-        <label>银行卡信息:</label>
-        <span>{{cardInfo}}</span>
-      </div>
-      <div id="citi-account">
-        <label>花旗账号:</label>
-        <button v-show="!isBinded">去绑定</button>
-        <span v-show="isBinded">{{citiAccount}}</span>
-
-      </div>
-
+      <Table
+        stripe
+        border
+        :show-header="noheader"
+        :columns="tbcolumns"
+        :data="tbdata"
+        class="tb">
+        <template slot-scope="{ row }" slot="c1">
+            <strong style="color: #035bc6;">{{ row.c1 }}</strong>
+        </template>
+        <template slot-scope="{ row }" slot="c3">
+            <strong style="color: #035bc6;">{{ row.c3 }}</strong>
+        </template>
+        <template slot-scope="{ row }" slot="c5">
+            <strong style="color: #035bc6;">{{ row.c5 }}</strong>
+        </template>
+      </Table>
     </form>
   </div>
 
 </template>
 
 <script>
-  export default {
+  export default{
     name: 'Account',
     data(){
       return {
@@ -52,31 +38,82 @@
         address: '163 xinlin road',
         cardInfo : 'ABC',
         citiAccount : 'citi',
-        isBinded: false //判断是否绑定了花旗账户
+        isBinded: false, //判断是否绑定了花旗账户
+
+        noheader: false,
+        tbcolumns: [
+          {
+            title: 'C1',
+            key: 'c1',
+            width: 100,
+            slot: 'c1'
+          },
+          {
+            title: 'C2',
+            key: 'c2',
+          },
+          {
+            title: 'C3',
+            key: 'c3',
+            width: 90,
+            slot: 'c3'
+          },
+          {
+            title: 'C4',
+            key: 'c4',
+          },
+          {
+            title: 'C5',
+            key: 'c5',
+            width: 120,
+            slot: 'c5'
+          },
+          {
+            title: 'C6',
+            key: 'c6',
+          }
+        ],
+        tbdata: []
       }
     },
     methods:{
       getInfo(){
         //通过api获取账户信息
+      },
+      tableShow() {
+        this.tbdata.push({
+          c1: '姓',
+          c2: this.firstName,
+          c3: '名',
+          c4: this.lastName,
+          c5: '手机号',
+          c6: this.phoneNumber
+        });
+        this.tbdata.push({
+          c1: '电子邮箱',
+          c2: this.email,
+          c3: '地址',
+          c4: this.address,
+          c5: '银行卡信息',
+          c6: this.cardInfo
+        });
+        this.tbdata.push({
+          c1: '花旗账号',
+          c2: this.citiAccount,
+          c3: '',
+          c4: '',
+          c5: '',
+          c6: ''
+        });
       }
-    }
+    },
   }
 </script>
 
 <style scoped>
-
-  h2{
-    margin: 30px;
-    margin-left: 60px;
-  }
-  label{
-    display: inline-block;
-    width: 6em;
-    text-align: right;
-  }
-  div{
-    margin: 20px;
-    font-size: 25px;
+.tb {
+  margin: 5px 30px;
 
 }
+
 </style>
