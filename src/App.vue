f3dfa2fcaab85f09f295179bb37424bef1f20e45
lollipop190 <191250190@smail.nju.edu.cn>
2020-10-08 12:58:47 +0800

    remove the comp and prepare for router usage

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
-<!--    <img src="./assets/logo.png">-->
-<!--    todo router-configuration-->
-<!--    <router-view/>-->
-
-    <Account/>
+    <router-view/>
 
   </div>
 
 </template>
 
 <script>
-  import Account from './components/AccountComps/Account.vue'
+
 export default {
   name: 'App',
-  components:{
-    Account
-  }
+
 }
 </script>
 
